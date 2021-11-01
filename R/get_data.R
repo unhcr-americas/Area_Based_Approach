0449e4d9ddc574d07a0b597c1ee7b9df5e136077
Edouard <edouard.legoupil@gmail.com>
2021-11-01 10:30:45 -0500

    rev

---
+++ R/get_data.R
@@ -0,0 +1,45 @@
+
+#' data_raw_dir
+#' 
+#' This function creates a data folder if one doesn't already exist.
+#'
+#' @md
+#' @examples
+#' \dontrun{
+#' }
+#' @export
+
+
+data_raw_dir <- function() {
+  if(!dir.exists("data-raw")) dir.create("data-raw")
+}
+
+#' get_data
+#' 
+#' Get all country specific dataset 
+#'
+#' @md
+#' @param src list of direct URL to dataset - dataset name to be normalised
+#'  with 3 letters country code plus year 
+#'   
+#' @examples
+#' \dontrun{
+#' }
+#' @export
+
+get_data <- function( ){
+  
+
+  #require(tidyverse)
+  #require(haven)
+  # library(foreign) # needed to import STATA files
+  # library(plyr)
+  # library(ggplot2)
+  # library(stringr)
+  # library(reshape2)
+  # library(RColorBrewer)
+  # library(stringi)  
+  
+ 
+}
+
